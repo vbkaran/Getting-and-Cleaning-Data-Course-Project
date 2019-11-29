ed5ffa67721262cdade188e09254a93ae5eb6558
vbkaran <vbk.singh@gmail.com>
2019-11-29 19:54:45 +0100

    second

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,4 @@
-#******************************************************************
 #Step Zero. Downloading and unzipping dataset
-#******************************************************************
 
 if(!file.exists("./data")){dir.create("./data")}
 #Here are the data for the project:
@@ -10,30 +8,29 @@ download.file(fileUrl,destfile="./data/Dataset.zip")
 # Unzip dataSet to /data directory
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
-#******************************************************************
-#Step 1.Merges the training and the test sets to create one data set.
-#******************************************************************
+
+#*****Step 1.Merges the training and the test sets to create one data set.
 
 # 1.1 Reading files
 
-# 1.1.1  Reading trainings tables:
+#  Trainings tables:
 
 x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
 subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
-# 1.1.2 Reading testing tables:
+# Testing tables:
 x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-# 1.1.3 Reading feature vector:
+# Feature vector:
 features <- read.table('./data/UCI HAR Dataset/features.txt')
 
-# 1.1.4 Reading activity labels:
+# Activity labels:
 activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
 
-# 1.2 Assigning column names:
+# 1.2 column names:
 
 colnames(x_train) <- features[,2]
 colnames(y_train) <-"activityId"
@@ -45,7 +42,7 @@ colnames(subject_test) <- "subjectId"
 
 colnames(activityLabels) <- c('activityId','activityType')
 
-#1.3 Merging all data in one set:
+#1.3 Merging 
 
 mrg_train <- cbind(y_train, subject_train, x_train)
 mrg_test <- cbind(y_test, subject_test, x_test)
@@ -54,8 +51,8 @@ setAllInOne <- rbind(mrg_train, mrg_test)
 #dim(setAllInOne)
 #[1] 10299   563
 
-#Step 2.-Extracts only the measurements on the mean and standard deviation for each measurement.
-#******************************************************************
+#*****Step 2.-Extracts only the measurements on the mean and standard deviation for each measurement.
+
 
 #2.1 Reading column names:
 
@@ -73,23 +70,18 @@ mean_and_std <- (grepl("activityId" , colNames) |
 
 setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
 
-#******************************************************************
-#Step 3. Uses descriptive activity names to name the activities in the data set
-#******************************************************************
+#*****Step 3. adjust descriptive activity names to name the activities
+
 
 setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
-                              by='activityId',
+                              by='activityId', 
                               all.x=TRUE)
 
-#******************************************************************
-#Step 4. Appropriately labels the data set with descriptive variable names.
-#******************************************************************
+#***** Step 4. added the descriptive variable names in part of the previous sections
 
-#Done part of the section 1, 2, 3 (see 1.3,2.2 and 2.3)
 
-#******************************************************************
-#Step 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-#******************************************************************
+
+#*****Step 5.  Average of each variable for each activity and each subject to arrange tidy data set
 
 #5.1 Making a tidy data set
 
